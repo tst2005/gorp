0e5e7460dd400fc0d77c3ec6008d43acc8dd9bd7
designintentional <jriddiough@gmail.com>
2013-01-07 00:59:03 +0000

    documentation for init

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,22 +1,43 @@
+-- AddCSLuaFile( String FileName )
+-- Sending to the clients the files they need
+-- http://maurits.tv/data/garrysmod/wiki/wiki.garrysmod.com/indexe9af-2.html
+--
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
+
 
 include("shared.lua")
 include("player.lua")
 include("db.lua")
 
+-- GM:PlayerConnect( String name, String address ) 
+-- Called whenever a player connects
+-- http://maurits.tv/data/garrysmod/wiki/wiki.garrysmod.com/index0d60.html
+--
 function GM:PlayerConnect(name, ip)
 	print ("Player " .. name .. "has joined the game.")
 end
 
+--  GM:PlayerInitialSpawn( Player Player )
+--  Is called when the player first spawns into the world. 
+--  http://maurits.tv/data/garrysmod/wiki/wiki.garrysmod.com/index2901.html
+--
 function GM:PlayerInitialSpawn(ply)
 	print ("Player " .. ply:Nick() .. "has spawned.")
 end
 
+--  GM:PlayerSpawn( Player Player ) 
+--  Called whenever a player respawns. 
+--  http://maurits.tv/data/garrysmod/wiki/wiki.garrysmod.com/index249d.html
+--
 function GM:PlayerSpawn(ply)
 	ply:SetModel("models/player/group01/male_07.mdl")
 end
 
+--  GM:PlayerAuthed( Player ply, String SteamID, Integer UniqueID ) 
+--  Called whenever the player is Authed with Steam and recieves the SteamID 
+--  http://maurits.tv/data/garrysmod/wiki/wiki.garrysmod.com/indexc4b1.html
+--
 function GM:PlayerAuthed(ply, steamID, uniqueID)
 	print ("Player " .. ply:Nick() .. "has authenticated.")
 end
